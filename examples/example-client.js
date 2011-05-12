cf1b1b48400614c3479f10d0a78d0e3a7206aec9
Gregory McWhirter <greg@hallofkvasir.org>
2011-05-12 14:15:57 -0700

    Updating things to be more streamlined. Making a working example.

--- examples/example-client.js
+++ examples/example-client.js
@@ -2,48 +2,52 @@
   File:      example-client.js
   
   Author:    Micheil Smith
+             Gregory McWhirter
   Description:
     Demonstration of the smtp library.
 ===============================================*/
-var sys = require("sys");
+var sys = require("util");
 var start_mem = process.memoryUsage();
 
 sys.puts(JSON.stringify(start_mem));
 
-var config = require("./config");
-var smtp = require("../lib/smtp");
+//var smtp = require('smtp');
+var smtp = require("../index");
+var to = "you@example.com";
 
-var client = new smtp.Client();
+var client = new smtp({host: "localhost",
+        port: 25,
+        from_addr: "you@example.com",
+        secure: false,
+        debug: true,
+        auth_required: false,
+        user: "",
+        pass: ""});
 
-var message = "Date: "+(new Date()).toString()+"\n\
-From: Node-SMTP <"+config.from+">\n\
-To: "+config.to+"\n\
-Subject: Node SMTP Works!\n\
-So, It looks like node-smtp all works, which is great news!\n\
+var message = "So, It looks like node-smtp all works, which is great news!\n\
 \n\
 Your's Truly,\n\
 SMTP Client.";
 
-client.addListener("packetSent", function(data){
-  sys.puts(">>> "+data);
-});
+var themail = {
+    from: "Node-SMTP",
+    to: to,
+    body: message,
+    subject: "Node SMTP Works!"
+};
 
-client.connect(config.port, config.host).addCallback(function(){
-  client.mail(config.from).addCallback(function(){
-    client.rcpt(config.to).addCallback(function(){
-      client.data(message).addCallback(function(){
-        client.quit().addCallback(function(){
-          client.disconnect();
-          
-          var mem = process.memoryUsage();
-					sys.puts(JSON.stringify(mem));
-					sys.puts(mem["rss"]-start_mem["rss"]);
-					sys.puts(mem["vsize"] - start_mem["vsize"]);
-					sys.puts(mem["heapTotal"] - start_mem["heapTotal"]);
-          sys.puts(mem["heapUsed"] - start_mem["heapUsed"]);
-          
-        });
-      });
-    });
-  });
+client.send(themail, function (successful){
+    if (successful){
+        sys.puts("Mail sent successfully.");
+    }
+    else {
+        sys.puts("Mail failed to be sent.");
+    }
+    
+    var mem = process.memoryUsage();
+    sys.puts(JSON.stringify(mem));
+    sys.puts(mem["rss"]-start_mem["rss"]);
+    sys.puts(mem["vsize"] - start_mem["vsize"]);
+    sys.puts(mem["heapTotal"] - start_mem["heapTotal"]);
+    sys.puts(mem["heapUsed"] - start_mem["heapUsed"]);
 });
--- index.js
+++ index.js
@@ -2,152 +2,131 @@
 // Please see the LICENSE file for licensing terms.
 //
 
-var smtp = require('./lib/smtp'),
-    sys = require('sys');
+var Client = require('./lib/client'),
+    sys = require('util');
 
-module.exports = function (){
-    return {
-        "_config": {
-            host: "localhost",
-            port: 25,
-            from_addr: "",
-            secure: false,
-            debug: false,
-            auth_required: false,
-            user: "",
-            pass: ""
-        },
-        
-        setConfig: function (new_config){
-            this._config = new_config;
-        },
-        
-        send: function (from, to, subject, body, cc, bcc, headers, callback){
-            var client = new smtp.Client();
-            
-            if (typeof(cc) == "function")
-            {
-                callback = cc;
-                cc = bcc = false;
-                headers = {};
-            }
-            else if (typeof(bcc) == "function")
-            {
-                callback = bcc;
-                bcc = false;
-                headers = {};
-            }
-            else if (typeof(headers) == "function")
-            {
-                callback = headers;
-                headers = {};
-            }
-            
-            rcpt_list = [];
-            
-            headers = headers || {};
-            headers["Date"] = (new Date()).toString();
-            headers["From"] = from + " <"+this._config.from_addr+">";
-            if (to instanceof Array)
-            {
-                headers["To"] = to.join(", ");
-                to.forEach(function (item){
-                    rcpt_list.push(item);
-                });
-            }
-            else
-            {
-                headers["To"] = to;
-                rcpt_list.push(to);
-            }
-            if (cc){
-                if (cc instanceof Array)
-                {
-                    headers["Cc"] = cc.join(", ");
-                    cc.forEach(function (item){
-                        rcpt_list.push(item);
-                    });
-                }
-                else
-                {
-                    headers["Cc"] = cc;
-                    rcpt_list.push(cc);
-                }
-                
-            } 
-            if (bcc){
-                if (bcc instanceof Array)
-                {
-                    headers["Bcc"] = bcc.join(", ");
-                    bcc.forEach(function (item){
-                        rcpt_list.push(item);
-                    });
-                }
-                else
-                {
-                    headers["Bcc"] = bcc;
-                    rcpt_list.push(bcc);
-                }
-                
-            } 
-            headers["Subject"] = subject;
-            
-            var message = '';
-            Object.keys(headers).forEach(function (key){
-                message += key+": "+headers[key]+"\r\n";
-            });
-            message += "\r\n"+body;
-            
-            var self = this;
-            
-            client.connect(this._config.port, this._config.host, this._config.secure, function(){
-                var send = function (){
-                    client.mail(self._config.from_addr, function(){
-                        client.rcptList(rcpt_list, function(){
-                            client.data(message, function(){
-                                client.quit(function(){
-                                    client.disconnect();
-                                    callback(true);
-                                });
-                            });
-                        });
-                    });
-                };
-                if (self._config.auth_required)
-                {
-                    client.auth(self._config.user, self._config.pass, send);
-                }
-                else
-                {
-                    send();
-                }
-            });
-            
-            if (this._config.debug)
-            {
-                client.addListener("packetSent", function (data){
-                    sys.debug(">>> "+data);
-                });
-            }
-            
-            client.addListener("error", function(err, context){
-                context = context || "packet";
-                
-                if (context == "quit")
-                {
-                    client.disconnect(function (){callback(false);});
-                }
-                else if (context == "disconnect")
-                {
-                    callback(false);
-                }
-                else
-                {
-                    client.quit(function (){
-                        client.disconnect(function (){callback(false);});
-                    });
-                }
+var SMTP = module.exports = function (config){
+    this._config = {
+        host: "localhost",
+        port: 25,
+        from_addr: "",
+        secure: false,
+        debug: false,
+        auth_required: false,
+        user: "",
+        pass: ""
+    };
+
+    if (config){
+        for (var key in config){
+            this._config[key] = config[key];
+        }
+    }
+}
+
+SMTP.prototype.send = function (mail, callback){
+    var client = new Client();
+    var rcpt_list = [];
+    var self = this;
+    var headers = mail.headers || {};
+    var message = '';
+
+    client.addListener("error", function(err, context){
+        context = context || "packet";
+
+        if (context == "quit")
+        {
+            client.disconnect(function (){callback(false, err);});
+        }
+        else if (context == "disconnect")
+        {
+            callback(false, err);
+        }
+        else
+        {
+            client.quit(function (){
+                client.disconnect(function (){callback(false, err);});
             });
         }
-    };
-};
+    });
+
+    if (this._config.debug)
+    {
+        client.addListener("packetSent", function (data){
+            sys.debug(">>> "+data);
+        });
+    }
+
+    headers["Date"] = (new Date()).toString();
+    headers["From"] = mail.from + " <"+this._config.from_addr+">";
+
+    if (mail.to instanceof Array){
+        headers["To"] = mail.to.join(", ");
+        mail.to.forEach(function (item){
+            rcpt_list.push(item);
+        });
+    }
+    else {
+        headers["To"] = mail.to;
+        rcpt_list.push(mail.to);
+    }
+
+    if (mail.cc){
+        if (mail.cc instanceof Array){
+            headers["Cc"] = mail.cc.join(", ");
+            mail.cc.forEach(function (item){
+                rcpt_list.push(item);
+            });
+        }
+        else
+        {
+            headers["Cc"] = mail.cc;
+            rcpt_list.push(mail.cc);
+        }
+    }
+
+    if (mail.bcc){
+        if (mail.bcc instanceof Array)
+        {
+            headers["Bcc"] = mail.bcc.join(", ");
+            mail.bcc.forEach(function (item){
+                rcpt_list.push(item);
+            });
+        }
+        else
+        {
+            headers["Bcc"] = mail.bcc;
+            rcpt_list.push(mail.bcc);
+        }
+
+    }
+
+    headers["Subject"] = mail.subject;
+
+    Object.keys(headers).forEach(function (key){
+        message += key+": "+headers[key]+"\r\n";
+    });
+    message += "\r\n"+mail.body;
+
+    client.connect(this._config.port, this._config.host, this._config.secure, function (){
+        var send = function (){
+            client.mail(self._config.from_addr, function (){
+                client.rcptList(rcpt_list.reverse(), function(){
+                    client.data(message, function (){
+                        client.quit(function (){
+                            client.disconnect();
+                            callback(true);
+                        });
+                    });
+                });
+            });
+        };
+
+        if (self._config.auth_required){
+            client.auth(self._config.user, self._config.pass, send);
+        }
+        else {
+            send();
+        }
+    });
+}
